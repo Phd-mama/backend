eb47bd78f45979628436880a89d7b9a7292e9aa3
kenzonayandra <kenzonayandra@gmail.com>
2024-09-29 16:35:01 +0700

    refactor: fix path

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('experts/', views.expert_profile_list, name='expert_list'),
-    path('experts/<int:pk>/', views.expert_profile_detail, name='expert_detail'),
+    path('', views.expert_profile_list, name='expert_list'),  
+    path('<int:pk>/', views.expert_profile_detail, name='expert_detail'),
 ]
