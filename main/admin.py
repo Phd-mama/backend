f64c9db778830e6442c470515f6c58b932429eaa
kenzonayandra <kenzonayandra@gmail.com>
2024-10-02 21:38:01 +0700

    feat: add admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import ExpertProfile
 
-# Register your models here.
+# Register ExpertProfile untuk tampil di Django Admin
+admin.site.register(ExpertProfile)
